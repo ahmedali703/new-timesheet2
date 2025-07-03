6271e70be760d72e6f23664e3c9379e1be47777d
Ahmed Al-Saied <ahmed.cns2022@gmail.com>
2025-07-03 20:09:38 +0400

    Fix NextAuth Route Handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,4 +5,10 @@ import { authOptions } from '@/lib/auth';
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export async function GET(request: Request) {
+  return handler(request);
+}
+
+export async function POST(request: Request) {
+  return handler(request);
+}
